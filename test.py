e4d7a71843f5c3859784515683b200040ce22869
Daisy Ellis <daisyellis@mitigogroup.com>
2025-11-11 16:05:08 +0000

    End of Day updates

---
+++ test.py
@@ -0,0 +1,21 @@
+import unittest
+from calculator_app import Calculator
+
+class TestOperations(unittest.Testcase):
+    def test_sum(self)
+        calc = Calculator (10, 2)
+        self.assertEqual(calc.get_quotient(), 0, "the division is wrong")
+
+        if __name== "__main__":
+            unitest.main()
+
+import unittest
+from calculator_app import Calculator
+
+class TestOperations(unittest.Testcase):
+    def test_sum(self)
+        calc = Calculator (10, 2)
+        self.assertEqual(calc.get_difference(), 8, "the Answer is wrong")
+
+        if __name== "__main__":
+            unitest.main()
