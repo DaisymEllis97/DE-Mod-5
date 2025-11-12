07fc265de54620e56b2571140ebbcc47c4273b48
Daisy Ellis <daisy.ellis@mitigogroup.com>
2025-11-12 15:47:00 +0000

    final day update

---
+++ docker_demo/class_calculatorapp.py
@@ -0,0 +1,16 @@
+class Calculator:
+    def __init__(self,a, b):
+        self.a = a
+        self.b = b 
+    def get_sum(self):
+        return self.a - self.b
+    def get_difference(self):
+        return self.a - self.b
+    def get_quotient(self):
+        return self.a / self.b
+
+#iniaties the calculator
+myCalc = Calculator(2,4)
+
+#applies the method get_product
+print(myCalc.get_sum())
